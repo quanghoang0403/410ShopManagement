1900a10e5bdf3e7c76a638657fb17d60189132d9
LuongKimPhuong2611 <18521275@gm.uit.edu.vn>
2020-12-25 09:04:19 +0700

    updateDTO

--- DTO/AccountDTO.cs
+++ DTO/AccountDTO.cs
@@ -9,5 +9,47 @@ namespace DTO
 {
     public class AccountDTO
     {
+        public AccountDTO(string username, string passwordd, string permision, int id_Staff)
+        {
+            this.Username = username;
+            this.Passwordd = passwordd;
+            this.Permision = permision;
+            this.Id_Staff = id_Staff;
+        }
+
+        public AccountDTO(DataRow row)
+        {
+            this.Username = row["Username"].ToString();
+            this.Passwordd = row["Passwordd"].ToString();
+            this.Permision = row["Permision"].ToString();
+            this.Id_Staff = (int)Convert.ToInt32(row["Id_Staff"].ToString());
+        }
+
+        private string username;
+        private string passwordd;
+        private string permision;
+        private int id_Staff;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Passwordd
+        {
+            get { return passwordd; }
+            set { passwordd = value; }
+        }
+        public string Permision
+        {
+            get { return permision; }
+            set { permision = value; }
+        }
+        public int Id_Staff
+        {
+            get { return id_Staff; }
+            set { id_Staff = value; }
+        }
     }
 }
--- DTO/ShipmentDetailDTO.cs
+++ DTO/ShipmentDetailDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,47 @@ namespace DTO
 {
     public class ShipmentDetailDTO
     {
+        public ShipmentDetailDTO(int id_ShipmentDetail, int id_Shipment, int id_Product, int quantity_Product)
+        {
+            this.Id_ShipmentDetail = id_ShipmentDetail;
+            this.Id_Shipment = id_Shipment;
+            this.Id_Product = id_Product;
+            this.Quantity_Product = quantity_Product;
+        }
+
+        public ShipmentDetailDTO(DataRow row)
+        {
+            this.Id_ShipmentDetail = (int)Convert.ToInt32(row["Id_ShipmentDetail"].ToString());
+            this.Id_Shipment = (int)Convert.ToInt32(row["Id_Shipment"].ToString());
+            this.Id_Product = (int)Convert.ToInt32(row["Id_Product"].ToString());
+            this.Quantity_Product = (int)Convert.ToInt32(row["Quantity_Product"].ToString());
+        }
+
+        private int id_ShipmentDetail;
+        private int id_Shipment;
+        private int id_Product;
+        private int quantity_Product;
+
+        public int Id_ShipmentDetail
+        {
+            get { return id_ShipmentDetail; }
+            set { id_ShipmentDetail = value; }
+        }
+
+        public int Id_Shipment
+        {
+            get { return id_Shipment; }
+            set { id_Shipment = value; }
+        }
+        public int Id_Product
+        {
+            get { return id_Product; }
+            set { id_Product = value; }
+        }
+        public int Quantity_Product
+        {
+            get { return quantity_Product; }
+            set { quantity_Product = value; }
+        }
     }
 }
